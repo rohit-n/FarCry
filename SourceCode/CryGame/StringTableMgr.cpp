40f2a9d3d623ceb8563d28bcf551534b649f2d12
Rohit Nirmal <rohitnirmal9@gmail.com>
2024-07-19 00:13:39 -0500

    Fix AppendToUnicodeString outputting garbage on Linux.

--- SourceCode/CryGame/StringTableMgr.cpp
+++ SourceCode/CryGame/StringTableMgr.cpp
@@ -531,7 +531,7 @@ void CStringTableMgr::AppendToUnicodeString(const string& sSource, wstring &sDes
 	swTemp.resize(sSource.size()+1);
 
 #if defined(LINUX)
-	swprintf (&swTemp[0], swTemp.size(), L"%S", sSource.c_str());
+	swprintf (&swTemp[0], swTemp.size(), L"%s", sSource.c_str());
 #else
 	swprintf (&swTemp[0], L"%S", sSource.c_str());
 #endif
